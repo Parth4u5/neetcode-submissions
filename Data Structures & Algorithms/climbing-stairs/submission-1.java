e4f5902d280abbc13f1dc8dd9b4623d0d202a8e0
ParthThanki <parththanki822@gmail.com>
2026-08-16 18:52:48 +0530

    Add comments to clarify climbing stairs solution
    Added comments explaining the climbing stairs problem and the dynamic programming approach.

--- Data Structures & Algorithms/climbing-stairs/submission-1.java
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -1,3 +1,9 @@
+// here this the problem of climbing stairs 
+// contraint is you can climb either 1 or 2 stairs 
+// which is standard fibbonaci problem 
+// ways(i) =  ways(i-1) + ways(i-2) 
+// to reach at i you need to be at either i-1 or i-2 
+// here also We have used traditional Dynamic Programming approach to store current result at each step to avoid space complexity as well as time complexity 
 class Solution {
     public int climbStairs(int n) {
         if(n<=2){
