c3d11eaa19df645613360122986eda0b2ebba164
ParthThanki <parththanki822@gmail.com>
2026-08-16 19:05:07 +0530

    Add method to get concatenation of an array
    Implemented method to concatenate an array with itself.

--- Data Structures & Algorithms/concatenation-of-array/submission-0.java
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -1,12 +1,17 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
+        // This is common sense that after merging two arrays size will become double of its normal size
         int newArray[] = new int[2*nums.length];
 
         int i=0;
         for(int num : nums){
+            // we are storing nums elements in newArray and  updating  to next position
             newArray[i++] = num;
         }
+        
         for(int num : nums){
+            //it is like we are doubling the same array 
+            // Actually we are not doing merging of two different arrays we are just doubling same array into newArray
             newArray[i++] = num;
         }
         return newArray;
