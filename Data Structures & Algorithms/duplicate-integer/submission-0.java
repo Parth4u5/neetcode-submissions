5dd4660c2fa9a33808eea245cd994b64773353c3
ParthThanki <parththanki822@gmail.com>
2026-08-16 19:16:20 +0530

    Add duplicate detection method using HashSet
    Implemented a method to check for duplicate integers using a HashSet.

--- Data Structures & Algorithms/duplicate-integer/submission-0.java
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -1,12 +1,18 @@
 class Solution {
+    // we are creating an hashset to store entry on those numbers 
+    // whose occurences are already marked
     HashSet<Integer> set = new HashSet<>();
     public boolean hasDuplicate(int[] nums) {
         for(int i=0;i<nums.length;i++){
+            //now check if perticular number existing in table 
             if(set.contains(nums[i])){
+                // if yes then return true
                 return true;
             }
+            //else add that value into hashtable
             set.add(nums[i]);
         }
+        //else return false
         return false;
     }
 }
