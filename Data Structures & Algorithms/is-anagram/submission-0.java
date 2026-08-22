341e04754e3bf5fe817ea1b8c7e8321585314226
ParthThanki <parththanki822@gmail.com>
2026-08-22 11:14:22 +0530

    Update submission-0.java

--- Data Structures & Algorithms/is-anagram/submission-0.java
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
+        //two hashmaps for two different strings
         HashMap<Character , Integer> smap = new HashMap<>();
         HashMap<Character , Integer> tmap = new HashMap<>();
 
-        for(char ch : s.toCharArray()){
-            smap.put(ch , smap.getOrDefault(ch , 0)+1);
+        for(char ch : s.toCharArray()){//traveres through each character of string s
+            smap.put(ch , smap.getOrDefault(ch , 0)+1);//calculate the frequency of each character
         }
-        for(char ch : t.toCharArray()){
-            tmap.put(ch , tmap.getOrDefault(ch , 0)+1);
+        for(char ch : t.toCharArray()){//traverse through each character of string t
+            tmap.put(ch , tmap.getOrDefault(ch , 0)+1);//calculate the frequency of each character
         }
 
         if(smap.equals(tmap)){
