33291271cc268b5ed9ca8c2fd245adf62259df6d
ParthThanki <parththanki822@gmail.com>
2026-08-22 11:11:44 +0530

    Update submission-0.java

--- Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.java
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.java
@@ -7,14 +7,14 @@ class Solution {
             if(nums[mid] == target){
                 return mid;
             }
-            if(nums[left]<=nums[mid]){
+            if(nums[left]<=nums[mid]){// here it is saying that left half of the array is sorted 
                 if(nums[left]<=target && target < nums[mid]){
-                    right = mid - 1;
+                    right = mid - 1;//target relays in left side of the array so end will become now mid-1
                 }else{
-                    left = mid + 1;
+                    left = mid + 1;//or start from the other side of mid
                 }
-            }else{
-                if(nums[mid]<target && target <= nums[right]){
+            }else{//here it is saying that right half of the array is sorted
+                if(nums[mid]<target && target <= nums[right]){//find in that sorted part
                     left = mid + 1;
                 }else{
                     right = mid - 1;
@@ -24,3 +24,12 @@ class Solution {
         return -1;
     }
 }
+//DEBUG TESTS :
+//Array = {7 , 9 , 11 , 0 , 1 , 2 , 3}
+//Target = 1
+// left = 0 , right = 7
+//mid = 0 + (7-0)/2 = 0 + (7)/2 = 0 + 3 = 3 , 3rd index is mid
+//if nums[3] == 1 => 0 == 1 false , move forward
+//if nums[0]<=nums[3] => 0 <= 0 True means left half is sorted
+//if nums[0]<=1 && 1<=0 false => left = mid + 1 , means here 7 , 9 , 11 , 0 (These elements are elementaed from search) , now search in 1 , 2 , 3
+//continue with loop and you will find at the end target 1 
