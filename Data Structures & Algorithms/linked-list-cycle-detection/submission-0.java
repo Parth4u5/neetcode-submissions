41992a04eae3da59bbf78fb5725409cccbf94edd
ParthThanki <parththanki822@gmail.com>
2026-08-22 11:57:45 +0530

    Update submission-0.java

--- Data Structures & Algorithms/linked-list-cycle-detection/submission-0.java
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.java
@@ -10,13 +10,17 @@
  */
 
 class Solution {
+    //this slow fast pointers approach
     public boolean hasCycle(ListNode head) {
+        //slow pointer moves 1 index
         ListNode slowPointer = head;
+        //fast pointer moves 2 index
         ListNode fastPointer = head;
 
         while(fastPointer != null && fastPointer.next != null){
             slowPointer = slowPointer.next;
             fastPointer = fastPointer.next.next;
+            // here the concept is so straight when slow poiner moves 1 index and fast moves 2 index both eventually gonna meet at the same point because of uneven movement in the both pointers
             if(fastPointer == slowPointer){
                 return true;
             }
